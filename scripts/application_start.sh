9612d321339f03526960d8abb78083bf25ef84ed
bhaskar <bhaskar15435@gmail.com>
2021-05-31 16:02:36 +0530

    first commit

---
+++ scripts/application_start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo chmod -R 777 /home/ec2-user/app
+
+cd /home/ec2-user/app
+
+pip3 intall -r requirements.txt
+
+python app.py > app.out.log 2> app.err.log < /dev/null &
---
+++ scripts/application_stop.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo "Stopping any existing node servers"
